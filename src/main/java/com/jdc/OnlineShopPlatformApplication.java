d98ac70505917070e6e014b75b570c52402db24c
'ChunGyi-j' <'moethuchun@gmail.com'>
2020-03-18 15:12:18 +0630

    update

---
+++ src/main/java/com/jdc/OnlineShopPlatformApplication.java
@@ -0,0 +1,14 @@
+package com.jdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopPlatformApplication {
+
+	public static void main(String[] args) {
+		System.out.println();
+		SpringApplication.run(OnlineShopPlatformApplication.class, args);
+	}
+
+}
